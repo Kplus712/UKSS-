f7b847230feccd7b7b89a14fa7caa01cdb3f1dc6
Kelvin deogratias <kelvinplus712@gmail.com>
2025-11-11 20:45:26 +0300

    Update candidate.js

--- candidate.js
+++ candidate.js
@@ -1,14 +1,49 @@
 {
   "headBoy": [
-    {"name":"John M.", "img":"https://via.placeholder.com/60"},
-    {"name":"David S.", "img":"https://via.placeholder.com/60"},
-    {"name":"Michael A.", "img":"https://via.placeholder.com/60"},
-    {"name":"Kevin B.", "img":"https://via.placeholder.com/60"}
+    { "name": "John Mussa", "image": "https://via.placeholder.com/150" },
+    { "name": "David Peter", "image": "https://via.placeholder.com/150" },
+    { "name": "Anna Joseph", "image": "https://via.placeholder.com/150" },
+    { "name": "Grace Martin", "image": "https://via.placeholder.com/150" }
   ],
   "generalSecretary": [
-    {"name":"Mary K.", "img":"https://via.placeholder.com/60"},
-    {"name":"Paulina N.", "img":"https://via.placeholder.com/60"},
-    {"name":"Sarah T.", "img":"https://via.placeholder.com/60"},
-    {"name":"Peter L.", "img":"https://via.placeholder.com/60"}
+    { "name": "George Ally", "image": "https://via.placeholder.com/150" },
+    { "name": "Mary Kelvin", "image": "https://via.placeholder.com/150" }
+  ],
+  "otherPositions": [
+    {
+      "position": "Head Girl",
+      "candidates": [
+        { "name": "Sarah James", "image": "https://via.placeholder.com/150" },
+        { "name": "Joyce Peter", "image": "https://via.placeholder.com/150" }
+      ]
+    },
+    {
+      "position": "Sports Captain",
+      "candidates": [
+        { "name": "Elvis David", "image": "https://via.placeholder.com/150" },
+        { "name": "Anna Grace", "image": "https://via.placeholder.com/150" }
+      ]
+    },
+    {
+      "position": "Discipline Prefect",
+      "candidates": [
+        { "name": "Victor John", "image": "https://via.placeholder.com/150" },
+        { "name": "Mary Thomas", "image": "https://via.placeholder.com/150" }
+      ]
+    },
+    {
+      "position": "Time Keeper",
+      "candidates": [
+        { "name": "Peter Frank", "image": "https://via.placeholder.com/150" },
+        { "name": "Lucy Daniel", "image": "https://via.placeholder.com/150" }
+      ]
+    },
+    {
+      "position": "Environment Prefect",
+      "candidates": [
+        { "name": "Joseph Musa", "image": "https://via.placeholder.com/150" },
+        { "name": "Flora John", "image": "https://via.placeholder.com/150" }
+      ]
+    }
   ]
 }
