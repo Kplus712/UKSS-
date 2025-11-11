c7dd38e22d5620954341d3d03c9b5e48f4aef109
Kelvin deogratias <kelvinplus712@gmail.com>
2025-11-11 12:06:21 +0300

    Create candidate.js

---
+++ candidate.js
@@ -0,0 +1,14 @@
+{
+  "headBoy": [
+    {"name":"John M.", "img":"https://via.placeholder.com/60"},
+    {"name":"David S.", "img":"https://via.placeholder.com/60"},
+    {"name":"Michael A.", "img":"https://via.placeholder.com/60"},
+    {"name":"Kevin B.", "img":"https://via.placeholder.com/60"}
+  ],
+  "generalSecretary": [
+    {"name":"Mary K.", "img":"https://via.placeholder.com/60"},
+    {"name":"Paulina N.", "img":"https://via.placeholder.com/60"},
+    {"name":"Sarah T.", "img":"https://via.placeholder.com/60"},
+    {"name":"Peter L.", "img":"https://via.placeholder.com/60"}
+  ]
+}
